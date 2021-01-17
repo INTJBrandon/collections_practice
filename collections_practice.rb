959ddb770cd6a8d814f09d9e5e9c5a7e98acbd4a
seireitou <ausis@mail.com>
2021-01-17 02:35:32 -0600

    commit tree 6cb7f0879f4451f77281821028af16b0c24c2bca

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,64 @@
+def sort_array_asc(array)
+    array.sort do |a,b|
+        a <=> b
+    end
+end
 
+def sort_array_desc(array)
+    array.sort do |a,b|
+        b <=> a
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |a,b|
+        a.size <=> b.size
+    end
+end
+
+def swap_elements(array)
+        a = array[1]
+        b = array[2]
+        array[1] = b
+        array[2] = a
+        array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.collect do |word|
+        word.slice!(2)
+        word.insert(2, '$')
+    end
+end
+
+def find_a(array)
+    arr = []
+    array.collect do |str|
+       if str[0] == "a"
+        arr << str
+       end
+    end
+    arr
+end
+
+
+def sum_array(array)
+    sum = 0
+    array.each do |num|
+    sum = sum + num
+    end
+    sum
+end
+
+def add_s(array)
+    string = array[1]
+    added = array.collect do |str|
+        str + "s"
+    end
+    added[1] = string
+    added
+end
